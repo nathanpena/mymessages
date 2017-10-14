997c4cf475d6f953a2e38bf1b85501f4ad2079e1
Nathan Pena <nnpena@outlook.com>
2017-10-14 13:58:16 -0400

    Added messages routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :accounts
+      resources :accounts do
+        resources :messages
+      end
     end
   end
 end
